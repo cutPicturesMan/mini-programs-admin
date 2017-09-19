005f23b1cbfb350439267d7d7eb4b5954cd598f1
臻 <408869403@qq.com>
2017-09-19 14:19:31 +0800

    四个tabs页面制作

---
+++ app.js
@@ -0,0 +1,25 @@
+
+App({
+  globalData: {
+
+  },
+
+  // onLaunch () {
+    // // 检查登录状态
+    // wx.checkSession({
+    //   success: () => {
+    //     console.log('checkSession 成功');
+    //     this.logs(new Date() + '登录成功');
+    //   },
+    //   fail: () => {
+    //     console.log('checkSession 失败');
+    //     // 如果失败，重新登录
+    //     this.login();
+    //   }
+    // });
+  // },
+
+  getUserInfo: function (cb) {
+
+  },
+})
---
+++ public/js/api.js
@@ -0,0 +1,35 @@
+let api = {
+
+  // 获取分类
+  category: '/category',
+  // 获取分类商品
+  category_products: '/category/products/',
+
+  // 获取某个商品
+  product: '/product/',
+  // 获取商品sku
+  product_sku: '/product/sku/',
+  // 商品搜索
+  product_search: '/product/search',
+
+  // 购物车
+  cart: '/cart',
+
+  // 订单
+  order: '/order/',
+
+  // 获取收货地址
+  address: '/address',
+  // 获取默认收货地址
+
+  // 登录
+  login: '/wx/login',
+  // 个人中心
+  user: '/wx/info'
+}
+
+for (var attr in api) {
+  api[attr] = 'https://www.byunfu.com/site' + api[attr];
+}
+
+export default api;
