6f69d587728152d489c27ed314297b491e537b02
your_name <your_email>
2017-10-22 11:38:14 +0800

    修改仓管库存功能

--- pages/store_remain/store_remain.js
+++ pages/store_remain/store_remain.js
@@ -5,8 +5,12 @@ Page({
   data: {
     // 订单列表
     list: [],
+    // 列表原始的库存数量，用于提交时比对
+    numList: [],
     // 数据是否加载完毕
-    isLoaded: false
+    isLoaded: false,
+    // 数据是否正在提交中
+    isSubmit: false
   },
   // 获取列表数据
   getData () {
@@ -16,10 +20,16 @@ Page({
       url: api.stock,
     }).then((res) => {
       wx.hideLoading();
+      let list = res.data;
+      let numList = [];
+      list.forEach((item) => {
+        numList.push(item.qtyAvailable);
+      });
 
       if (res.errorCode === 200) {
         this.setData({
-          list: res.data,
+          list,
+          numList,
           isLoaded: true
         });
       }
@@ -33,7 +43,7 @@ Page({
 
     num--;
     // 库存数量不能小于0
-    if(num < 0){
+    if (num < 0) {
       num = 0;
     }
 
@@ -50,7 +60,7 @@ Page({
     let num = e.detail.value;
 
     // 库存数量不能小于0
-    if(num < 0){
+    if (num < 0) {
       num = 0;
     }
 
@@ -71,6 +81,95 @@ Page({
 
     this.setData({
       list: list
+    })
+  },
+  // 提交
+  submit () {
+    let { list, numList, isSubmit } = this.data;
+    let modifyArr = [];
+
+    try {
+      if (isSubmit) {
+        throw new Error('正在修改中...');
+      }
+
+      // 循环列表，如果库存不一样，就表示修改了，需要写入数据库
+      list.forEach((item, index) => {
+        if (item.qtyAvailable != numList[index]) {
+          let obj = {
+            skuId: item.id,
+            num: item.qtyAvailable
+          };
+
+          modifyArr.push(obj);
+        }
+      })
+
+      if (modifyArr.length == 0) {
+        throw new Error('您未修改任何商品');
+      }
+    } catch (e) {
+      return wx.showToast({
+        title: e.message,
+        image: '../../icons/close-circled.png',
+        duration: 4000
+      })
+    }
+
+    this.setData({
+      isSubmit: true
+    })
+
+    // 数据转化格式，然后提交
+    let keys = [];
+    let values = [];
+    modifyArr.forEach((item, index) => {
+      keys.push(`skus[${index}].skuId`, `skus[${index}].num`);
+      values.push(item.skuId, item.num);
+    });
+
+    let skus = {};
+    keys.forEach((item, index) => {
+      skus[item] = values[index];
+    });
+
+    wx.showLoading();
+    http.request({
+      url: api.stock,
+      method: 'PUT',
+      data: {
+        ...skus
+      }
+    }).then((res) => {
+      // 提交成功，则跳转到待处理页面
+      if (res.errorCode === 200) {
+        wx.showToast({
+          title: res.moreInfo || '提交成功'
+        })
+
+        setTimeout(() => {
+          this.setData({
+            isSubmit: false
+          })
+          this.getData();
+        }, 1500)
+      } else {
+        // 提交失败，则提示
+        wx.showToast({
+          title: res.moreInfo,
+          image: '../../icons/close-circled.png'
+        })
+
+        setTimeout(() => {
+          this.setData({
+            isSubmit: false
+          });
+        }, 1500)
+      }
+    }).catch(() => {
+      this.setData({
+        isSubmit: false
+      })
     })
   },
   onLoad () {
